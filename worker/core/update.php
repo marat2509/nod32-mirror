27247b211c9b3400fbc7fb3fea7eca6d83cefd2a
marat2509 <marat2509@users.noreply.github.com>
2024-02-25 01:09:16 +0300

    Initial version

---
+++ worker/core/update.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . "/inc/init.php";
+
+try {
+    Log::init();
+    Language::init();
+    Config::init();
+    Language::init();
+
+    @ini_set('memory_limit', Config::get('SCRIPT')['memory_limit']);
+
+    $nod32ms = new Nod32ms();
+}
+catch (ToolsException $e) {
+    Log::write_log($e->getMessage(), 0);
+}
+catch (ConfigException $e) {
+    Log::write_log($e->getMessage(), 0);
+}
+catch (phpmailerException $e) {
+    Log::write_log($e->getMessage(), 0);
+}
+catch (Exception $e) {
+    Log::write_log($e->getMessage(), 0);
+}
---
+++ worker/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -f "/app/health.ok" ]; then
+    rm -rf /app/health.ok
+fi
+
+health=""
+
+if [ ! -f "/app/nod32ms.conf" ]; then
+    echo "E: Config file not found"
+    echo "E: Create nod32ms.conf file in directory with docker-compose.yml file and restart"
+else
+    health="$health+"
+fi
+
+if [ -z "${UPDATE_INTERVAL}" ]; then
+    UPDATE_INTERVAL=3600
+fi
+
+if [ "${#health}" = "1" ]; then
+    touch /app/health.ok
+    watch -n $UPDATE_INTERVAL "php /app/update.php"
+else
+    while true; do
+        read _
+    done
+fi
