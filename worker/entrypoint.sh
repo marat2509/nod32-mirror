47db24e4006ea9a410916420fd229dfc546a8fdd
marat2509 <93652988+marat2509@users.noreply.github.com>
2024-03-05 01:35:35 +0300

    Merge dev in main (#4)
    Co-authored-by: marat2509 <marat2509@users.noreply.github.com>

--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 
-if [ -f "/app/health.ok" ]; then
-    rm -rf /app/health.ok
-fi
-
-health=""
-
 if [ ! -f "/app/nod32ms.conf" ]; then
     echo "E: Config file not found"
     echo "E: Create nod32ms.conf file in directory with docker-compose.yml file and restart"
-else
-    health="$health+"
+    exit 1
 fi
+
 
 if [ -z "${UPDATE_INTERVAL}" ]; then
     UPDATE_INTERVAL=3600
 fi
 
-if [ "${#health}" = "1" ]; then
-    touch /app/health.ok
-    watch -eptn $UPDATE_INTERVAL "php /app/update.php"
-else
-    while true; do
-        read _
-    done
-fi
+watch -n $UPDATE_INTERVAL "php /app/update.php"
